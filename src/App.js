ba3a469ca7d21d9d19dc5eccb610038591fea20f
Rutik Gagare <rutikgagare091971@gmail.com>
2023-03-31 14:42:51 +0530

    folder structure changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import CourseGoalList from './components/CourseGoals/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput';
 import './App.css';
 
 const App = () => {
